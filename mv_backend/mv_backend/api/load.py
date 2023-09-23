9404e4ecf73aacb336e168fa8cb27811c45acb84
dotGeon <dotgeon@yandex.com>
2023-09-23 18:15:42 +0900

    fix: added common body_unicode and json processing to load.py

--- mv_backend/mv_backend/api/load.py
+++ mv_backend/mv_backend/api/load.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import path
@@ -5,6 +6,11 @@ from django.urls import path
 from mv_backend.api.function.hello_world import call as hello_world
 
 from mv_backend.api.function.gpt import call as gpt
+
+def get_body_from_request(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    return body
 
 def api_render(name, output):
     return path(
