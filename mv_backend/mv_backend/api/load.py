3ec694d1475f42c4a5fa9f2494d1b1e574708735
kdonghyun13 <kdonghyun13@g.skku.edu>
2023-10-03 20:34:56 +0900

    Update load.py
    reflect api path setting

--- mv_backend/mv_backend/api/load.py
+++ mv_backend/mv_backend/api/load.py
@@ -7,6 +7,8 @@ from mv_backend.api.function.hello_world import call as hello_world
 
 from mv_backend.api.function.gpt import call as gpt
 
+from mv_backend.api.function.reflect import call as reflect
+
 def api_render(name, output):
     return path(
         'api/' + name + '/',
@@ -17,5 +19,6 @@ def api_render(name, output):
 def api_path():
     return [
         api_render('test', hello_world),
-        api_render('gpt', gpt)
+        api_render('gpt', gpt),
+        api_render('reflect', reflect)
     ]
