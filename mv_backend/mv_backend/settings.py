e0f0e97284a06abeaae2b51cea910f89eaf4b651
lyh030725 <lyh030725@gmail.com>
2023-10-04 14:28:21 +0900

    fix: connect backend

--- mv_backend/mv_backend/settings.py
+++ mv_backend/mv_backend/settings.py
@@ -64,7 +64,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     'chrome-extension://gmmkjpcadciiokjpikmkkmapphbmdjok',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://skkucorp2023.geonhui.com/'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
