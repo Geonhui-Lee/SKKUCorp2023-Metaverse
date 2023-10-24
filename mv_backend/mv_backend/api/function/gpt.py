e09244cefd994d549835a7eee6b7d3f499258205
kdonghyun13 <kdonghyun13@g.skku.edu>
2023-10-24 18:15:02 +0900

    gpt.py langchain

--- mv_backend/mv_backend/api/function/gpt.py
+++ mv_backend/mv_backend/api/function/gpt.py
@@ -1,22 +1,50 @@
 from django.http import HttpResponse, JsonResponse
 from mv_backend.settings import OPENAI_API_KEY
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 import json, openai
+
+openai.api_key = OPENAI_API_KEY
+import os
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+chat = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
+
+query_template = """
+You are a customer at a pizza restaurant. Have a conversation appropriate to the situation, such as ordering pizza.
+previuos conversation: {conversation}
+"""
+query_prompt = PromptTemplate(
+    input_variables=["conversation"], template=query_template
+)
+
+query = LLMChain(
+    llm=chat,
+    prompt=query_prompt
+)
 
 def call(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
 
-    openai.api_key = OPENAI_API_KEY
-    openai_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=body["messages"]
-    )
-    openai_response_message = openai_response["choices"][0]["message"]
-
+    # openai.api_key = OPENAI_API_KEY
+    # openai_response = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=body["messages"]
+    # )
+    # openai_response_message = openai_response["choices"][0]["message"]
+    messages=body["messages"]
+    
     messages_response = body["messages"] + [
         {
             "role": "assistant",
-            "content": openai_response_message["content"]
+            "content": query.run(conversation = messages)
         }
     ]
     
