02ee31e9d0eae7137998d951dd3502a61ddab7f8
kdonghyun13 <kdonghyun13@g.skku.edu>
2023-11-06 13:18:29 +0900

    Update reflect_auto_test.py

--- mv_backend/mv_backend/api/function/reflect_auto_test.py
+++ mv_backend/mv_backend/api/function/reflect_auto_test.py
@@ -97,7 +97,7 @@ def reflect(npc, user):
     for chat_data in conversation:
         data_num += 1
         all_chat_data.append(chat_data["name"] + ": " + chat_data['memory'])
-        all_chat_data_node.append("[" + str(data_num) + "] " + chat_data["name"] + ": " + chat_data)
+        all_chat_data_node.append("[" + str(data_num) + "] " + chat_data["name"] + ": " + chat_data['memory'])
         all_chat_data.append(chat_data)
         all_chat_data_string += chat_data["name"] + ": " + chat_data['memory'] + "\n"
     
@@ -174,7 +174,7 @@ def reflect(npc, user):
         recency *= 0.995
         
         # chat_data_score["[" + str(data_num) + "]" + chat_data] += 0.1*score + recency
-        chat_data_score["[" + str(data_num) + "] " + chat_data] += recency
+        chat_data_score["[" + str(data_num) + "] " + chat_data['memory']] += recency
 
     sorted_dict = sorted(chat_data_score.items(), key = lambda item: item[1], reverse = True)
     print(sorted_dict)
