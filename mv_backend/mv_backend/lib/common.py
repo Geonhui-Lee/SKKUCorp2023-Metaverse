acf389a809c162ca919fefbd3ece041f20591ae0
dotGeon <dotgeon@yandex.com>
2023-11-10 15:30:11 +0900

    fix: common openai api key

--- mv_backend/mv_backend/lib/common.py
+++ mv_backend/mv_backend/lib/common.py
@@ -1,6 +1,8 @@
 from langchain.chat_models import ChatOpenAI
+from mv_backend.settings import OPENAI_API_KEY
+import openai
 
+openai.api_key = OPENAI_API_KEY
 gpt_model_name = 'gpt-4-1106-preview'
-
 def CommonChatOpenAI():
     return ChatOpenAI(model_name=gpt_model_name, temperature=0)
