21ffb734b150e21fe82fe1d7e6968d1f63d186a7
lyh030725 <lyh030725@gmail.com>
2023-11-17 15:27:55 +0900

    fix

--- mv_backend/mv_backend/api/function/chat.py
+++ mv_backend/mv_backend/api/function/chat.py
@@ -49,10 +49,10 @@ user's character: {reflect}
 user is bad at: {retrieve}
 you **always** *suggest* a user answer that the user can understand by *referring* *user's bad* and that fits the user's conversation style.
 You should always have a conversation about your job.
-But, If user doesn't seem interested in a conversation, induce a conversation about user's interests, *keeping concept of your job*.
+But, *Only If* user doesn't seem interested in a conversation, induce a conversation about user's interests, *keeping concept of your job*.
 
 If user is unable to answer:
-    *Ask* the user if they don't understand the question, and if so, You have to *suggest* a user answer along with advice to the user by *using* user's bad.
+    First, *MUST* *Ask* the user if they don't understand the question, and if so, You have to *suggest* a user answer along with advice to the user by *using* user's bad.
 
 previous conversation:
 {summary}
