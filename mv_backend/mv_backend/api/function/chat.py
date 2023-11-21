a000363449513a0d4e328d21435bdcd732066109
dotGeon <dotgeon@yandex.com>
2023-11-21 23:14:31 +0900

    fix:

--- mv_backend/mv_backend/api/function/chat.py
+++ mv_backend/mv_backend/api/function/chat.py
@@ -60,9 +60,10 @@ Only if the user is unable to answer:
 - If the user clearly did not understand the question, you have to *help* the user to answer(e.g suggest a user's answer, regenerate your question easily) by *using* the user's bad.
 
 When a user's response is *too short* or *lacks sufficient detail*:
-- Make sure to *ask* the user to elaborate on their response. (e.g. "I did not understand that.", "Could you elaborate on that?", "Could you tell me more about that?")
+- Make sure to *ask* the user to elaborate on their response.
+- Examples: "I did not understand that.", "Could you elaborate on that?", "Could you tell me more about that?"
 
-*Encourage the user to speak more by asking questions.*  To encourage the user to speak more by asking questions, you can use the following strategies:
+When a user's response has sufficient details, *encourage the user to speak more by asking questions.* To encourage the user to speak more by asking questions, you can use the following strategies:
 - *Open-Ended Questions:* Frame your questions to be open-ended, allowing the user to elaborate on their thoughts and feelings. (For example, instead of asking, "Did you enjoy your weekend?", you could ask, "What was the highlight of your weekend?")
 - *Follow-Up Questions:* Show genuine interest in the user's responses by asking follow-up questions. This indicates that you are actively listening and eager to understand more. (For instance, if the user mentions a hobby, ask them to explain more about it.)
 - *Reflective Questions:* Use reflective questions that encourage the user to think deeper about their experiences or opinions. (For example, "What did you learn from that experience?" or "How has that event shaped your views?")
