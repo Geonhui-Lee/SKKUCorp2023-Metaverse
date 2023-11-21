251c11c9622958676013343a8763a55cf4eb51f0
dotGeon <dotgeon@yandex.com>
2023-11-21 20:13:30 +0900

    fix:

--- mv_backend/mv_backend/api/function/chat.py
+++ mv_backend/mv_backend/api/function/chat.py
@@ -59,7 +59,13 @@ Only if the user is unable to answer:
 - First,*ask* the user to confirm whether the user does not understand the question.
 - If the user clearly did not understand the question, you have to *help* the user to answer(e.g suggest a user's answer, regenerate your question easily) by *using* the user's bad.
 
-Encourage the user to speak more by asking questions.
+Encourage the user to speak more by asking questions. The key is to maintain a balance between asking questions and listening attentively to the answers, ensuring the conversation remains engaging and comfortable for the user. To encourage the user to speak more by asking questions, you can use the following strategies:
+1. *Open-Ended Questions:* Frame your questions to be open-ended, allowing the user to elaborate on their thoughts and feelings. For example, instead of asking, "Did you enjoy your weekend?", you could ask, "What was the highlight of your weekend?"
+2. *Follow-Up Questions:* Show genuine interest in the user's responses by asking follow-up questions. This indicates that you are actively listening and eager to understand more. For instance, if the user mentions a hobby, ask them to explain more about it.
+3. *Reflective Questions:* Use reflective questions that encourage the user to think deeper about their experiences or opinions. For example, "What did you learn from that experience?" or "How has that event shaped your views?"
+4. *Clarifying Questions:* If the user mentions something unclear or intriguing, ask clarifying questions to get more details. This can also help avoid misunderstandings.
+5. *Personal Interest Questions:* Ask questions that relate to the user's interests, aspirations, or past experiences. This personal touch shows that you value their unique perspective.
+6. *Empathy-Driven Questions:* When appropriate, ask questions that demonstrate empathy and understanding. Phrases like "How did that make you feel?" or "What was going through your mind at that time?" can be very effective.
 
 Previous conversation:
     {summary}
