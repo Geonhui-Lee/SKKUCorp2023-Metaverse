12bd566728b5a55e46637b7a6b6475533f401816
Geonhui Lee <dotgeon@yandex.com>
2023-11-24 12:34:57 +0900

    fix:

--- mv_backend_cefr/src/cefr/cefr.service.ts
+++ mv_backend_cefr/src/cefr/cefr.service.ts
@@ -9,6 +9,32 @@ export class CEFRService {
   analyzeCEFR(analyzeCEFRDto: AnalyzeCEFRDto): CEFREvaluationDto {
     const contentToEvaluate = analyzeCEFRDto.content;
     const evaluation: CEFREvaluationDto = grade(contentToEvaluate);
+
+    function getNormalizedLevelValue(level: number): number {
+      //return level >= 100 ? 0 : level;
+      return level;
+    }
+    const levels = {
+      A1: getNormalizedLevelValue(evaluation.meta.levels.A1),
+      A2: getNormalizedLevelValue(evaluation.meta.levels.A2),
+      B1: getNormalizedLevelValue(evaluation.meta.levels.B1),
+      B2: getNormalizedLevelValue(evaluation.meta.levels.B2),
+      C1: getNormalizedLevelValue(evaluation.meta.levels.C1),
+      C2: getNormalizedLevelValue(evaluation.meta.levels.C2),
+    };
+    evaluation.meta.levels = levels;
+
+    const normalizedGrade = ['IDK'];
+    for (const level in levels) {
+      if (levels[level] >= 90) {
+        normalizedGrade.push(level);
+        break;
+      }
+    }
+    evaluation.meta.grade = normalizedGrade[
+      normalizedGrade.length - 1
+    ] as CEFREvaluationDto['meta']['grade'];
+
     return evaluation;
   }
 }
