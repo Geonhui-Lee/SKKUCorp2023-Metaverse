37d37fc113fddcc2e2e5bdf7e245ad0e94ee1ac1
kdonghyun13 <kdonghyun13@g.skku.edu>
2023-12-28 17:57:28 +0900

    retrieve.py refactoring

--- mv_backend/mv_backend/api/function/retrieve.py
+++ mv_backend/mv_backend/api/function/retrieve.py
@@ -1,32 +1,32 @@
-from django.http import HttpResponse, JsonResponse
-import json, openai
+import openai
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI
 import numpy as np
 from numpy.linalg import norm
 from langchain.embeddings import OpenAIEmbeddings
-import json, openai
 from datetime import datetime
 from bson.objectid import ObjectId
-from pymongo.mongo_client import MongoClient
 from mv_backend.lib.database import Database
 from mv_backend.lib.common import CommonChatOpenAI
+from mv_backend.settings import OPENAI_API_KEY
 
-OPENAI_API_KEY = "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"
 openai.api_key = OPENAI_API_KEY
 import os
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-MONGODB_CONNECTION_STRING = "mongodb+srv://geonhui:dotgeon@metaverse.px60xor.mongodb.net/?"
-
 db = Database()
 
 chat = CommonChatOpenAI()
-# every agents for this code
+
 embeddings_model = OpenAIEmbeddings()
 
-###
+# every prompts for this code
+
+# prompt: find 5 important conversations
+#
+# input:
+# {event} -- converstation converstation (user <-> npc)
+# {name} -- user's name
 generate_important_template = """
 Find five important dialogues in the following conversation for {name}.
 
@@ -44,9 +44,15 @@ generate_important = LLMChain(
     prompt=generate_important_prompt
 )
 
-# find the insight
-# !<INPUT 0>! -- Numbered list of event/thought statements
-# !<INPUT 1>! -- target persona name or "the conversation"
+# prompt: Create NPC answer to a query about the user
+# 
+# input:
+# {query} -- final_points (provides specific criteria)
+# {name} -- npc's name
+# {opponent} -- user's name
+# {event} -- converstation converstation (user <-> npc)
+# 
+# what the user is bad at: grammar, impolite or morally wrong
 generate_template = """
 Query:
 Find out what the *{opponent}* is bad at (grammar, impolite or morally wrong, etc.)
@@ -75,73 +81,43 @@ generate_retrieve = LLMChain(
     prompt=generate_prompt
 )
 
+# reflect:
+# -- Retrieve Phase --
+#   Find 15 conversations using 3 criteria
+#   3 criteria:
+#   relevance -- relevance to retrieve query (embedding and cosine similarity)
+#   recency -- conversation recency score (For previous conversations, start at 1 and multiply by 0.995)
+#   importance -- find important conversations
+#
+# -- Generate Phase -- 
+#   Generate NPC answer to a query about the user
+#
+# input:
+# npc -- npc's name
+# user -- user's name
+# chat_data_list -- conversation
 def retrieve(npc, user, chat_data_list):
+    data_num = 0                        # the number of data
+    all_chat_data = []                  # One session conversation (list)
+    all_chat_data_node = []             # One session conversation with node (list)
+    all_chat_data_string = ""           # One session conversation (string)
+    retrieve = ""                       # npc's answer (what the user is bad at)
 
-    ### mongoDB user's memory ###
-    # conversation = db.get_recent_documents(user, "Conversations")
-    data_num = 0
-
-    all_chat_data = []
-    all_chat_data_node = []
-    all_chat_data_string = ""
+    # Organize One session conversations
     for chat_data in reversed(chat_data_list):
         data_num += 1
         all_chat_data.append(chat_data)
         all_chat_data_node.append("[" + str(data_num) + "]" + chat_data)
-        # all_chat_data.append(chat_data)
         all_chat_data_string += chat_data + "\n"
     
+    # If the number of data is 0, return empty retrieve
     if data_num == 0:
-        return
-
-    # conversation = Database.get_all_documents(db, user, "conversations")
-    # data_num = 0
-
-    # all_chat_data = []
-    # before_chat_data = []
-    # all_chat_data_node = []
-    # important = []
-    # important_sum = 0
-    # for chat_data in conversation:
-    #     data_num += 1
-    #     before_chat_data.append(chat_data["name"] + ": " + chat_data["memory"])
-    #     # important.append(int(chat_data["important"]))
+        return retrieve
     
-    # # data_num = 0
-    # # for chat_data in reversed(important):
-    # #     data_num += 1
-    # #     important_sum += chat_data
-    # #     if data_num > 50:
-    # #         break
-    
-    # # if data_num == 0 or important_sum < 50:
-    # #     return
-    
-    # if data_num == 0:
-    #     return
-
-    # important_sum = 0
-    # data_num = 0
-    # for chat_data in reversed(before_chat_data):
-    #     data_num += 1
-    #     if data_num > 100:
-    #         break
-    #     all_chat_data_node.append("[" + str(data_num) + "]" + chat_data)
-    #     all_chat_data.append(chat_data)
-    # # all_chat_data_string = ""
-    # # # now reflect with 100 message data (we wil generate only one query)
-    # # data_num = 0
-    # # for chat_data in reversed(body["messages"]):
-    # #     data_num += 1
-    # #     if data_num > 100:
-    # #         break
-    # #     all_chat_data_node.append("[" + str(data_num) + "]" + chat_data["role"] + ": " + chat_data["content"])
-    # #     all_chat_data.append(chat_data["role"] + ": " + chat_data["content"])
-    # #     all_chat_data_string += chat_data["role"] + ": " + chat_data["content"] + "\n"
-    
-
+    # retrieve query (High-level question that NPC might think about during conversation)
     focal_points = "Find out what the user is bad at (grammar, understanding of context, etc.)"
-    ####프롬프트 추가 final_points#####
+    
+    # final_points prompt (provides specific criteria)
     final_points = """
     If the user has made a gramatical mistake conduct the following tasks.
     1. find and explain the gramatical mistakes the user has made with the specific grammar. (Except punctuation and capitalization)
@@ -152,55 +128,65 @@ def retrieve(npc, user, chat_data_list):
     1. find and explain the bad manners the user has made.
     2. *Always* show the *exact* sentence the user made the bad manners.
     """
+
+    # -- Retrieve Phase --
+    # query embedding (LangChain - Text embedding models - embed query)
     embedded_query = embeddings_model.embed_query(focal_points)
-    embedings = embeddings_model.embed_documents(all_chat_data_string)
+    # conversation embedding (LangChain - Text embedding models - embed documents)
+    embeddings = embeddings_model.embed_documents(all_chat_data_string)
 
-    cosine = np.dot(embedings, embedded_query)/(norm(embedings, axis=1)*norm(embedded_query))
-    print(cosine)
+    # relevance score: cosine similarity between two embeddings
+    cosine = np.dot(embeddings, embedded_query)/(norm(embeddings, axis=1)*norm(embedded_query))
 
+    # Binding conversation and relevance score
     chat_data_score = dict(zip(all_chat_data_node, cosine))
 
-    # retrieve to find 30 chat data with the generated query, embedding vetors, recency
     data_num = 0
-    recency = 1
-    # for score, chat_data in zip(reversed(important), all_chat_data):
+    recency = 1                         # recency score
+
+    # Calculate recency score and add it to the relevance score.
     for chat_data in all_chat_data:
         data_num += 1
         if data_num > 100:
             break
         recency *= 0.995
-        
-        # chat_data_score["[" + str(data_num) + "]" + chat_data] += 0.1*score + recency
+
         chat_data_score["[" + str(data_num) + "]" + chat_data] += recency
     
     sorted_dict = sorted(chat_data_score.items(), key = lambda item: item[1], reverse = True)
-    print(sorted_dict)
 
-    # find the insights with 30 important data
+    # Find 5 important conversations
     important_data_string = "[1] "
     important_data_string += generate_important.run(name = user, event = all_chat_data_string)
-    data_num = 0
+
+    # Find 10 conversations with the highest scores (relevance score + recency score)
+    data_num = 5
     for chat_data in sorted_dict:
         data_num += 1
-        if data_num > 10:
+        if data_num > 15:
             break
         important_data_string += chat_data[0] + "\n"
+    
+    # -- Generate Phase --
+    # Generate NPC insights about the user
     retrieve = generate_retrieve.run(query = final_points, name = npc + "'s", opponent = user, event = important_data_string)
 
     previous = Database.get_all_documents(db, user, "Retrieves")
-    print(previous)
     data_num = 0
-    node = 0
+    node = 0                            # data node
 
     for i in previous:
         data_num += 1
     
+    # Find the next node
     if data_num != 0:
         print(i)
         node = i["node"]
         node += 1
     
+    # timestamp
     datetimeStr = datetime.now().strftime("%Y-%m-%d")
+    # Save NPC insights to database
     document_user = {"_id":ObjectId(),"node":node,"timestamp":datetimeStr,"retrieve":retrieve,"name":npc}
     print(Database.set_document(db, user, "Retrieves", document_user))
 
